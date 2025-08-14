2d300874ac01dc6ef9467d82e6d3afe253a29bdd
Silveralitz <www.matias0902@gmail.com>
2025-08-14 19:12:59 -0300

    Contraseñayrepetircontraseñanovalidossinosoniguales

--- js/validacion.js
+++ js/validacion.js
@@ -1,6 +1,13 @@
 function showAlertSuccess() {
     document.getElementById("alert-success").classList.add("show");
 }
+// Validar coincidencia de contraseñas
+const password = document.getElementById("password").value;
+const password2 = document.getElementById("password2").value;
+
+if (password !== password2) {
+    esValido = false; // marca como inválido
+}
 
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
